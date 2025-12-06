668743d725197ab5a246b6f4c904d0465a87222c
tech1savvy <amankumar010604@gmail.com>
2025-12-06 19:17:31 +0530

    feat(mongodb): update connection logic

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const connectdb = require("./util/db/connect.js");
+const { connectdb } = require("./util/db/connect.js");
 const app = require("./app.js");
 
 const PORT = process.env.PORT | 8000;
@@ -8,8 +8,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 connectdb(MONGODB_URI);
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err.message);
-  }
-  console.log(`Server is listening on ${PORT}`);
+	if (err) {
+		console.log(err.message);
+	}
+	console.log(`Server is listening on ${PORT}`);
 });
--- server/util/db/connect.js
+++ server/util/db/connect.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 function connectdb(uri) {
-  mongoose
-    .connect(uri)
-    .then(() => {
-      console.log("Successfully connected to MongoDB");
-    })
-    .catch((err) => {
-      console.log("Failed to connect MongoDB");
-    });
+	mongoose
+		.connect(uri)
+		.then(() => {
+			console.log("Successfully connected to MongoDB");
+		})
+		.catch((err) => {
+			console.log(`Failed to connect MongoDB, error: + ${err}`);
+		});
 }
 
-module.exports = connectdb;
+module.exports = { connectdb };
