770cc826bd4246e70f62a53fb6004353118a03ca
tech1savvy <amankumar010604@gmail.com>
2025-12-07 09:49:36 +0530

    feat(seeder): add error handling to seeder script

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,5 +1,20 @@
+const { exit } = require("node:process");
 const { connectdb } = require("../util/db/connect.js");
 require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 connectdb(MONGODB_URI);
+
+const { seedUser } = require("./user.seed.js");
+
+// Async IIFE for calling seeders
+(async () => {
+	try {
+		await seedUser();
+	} catch (error) {
+		console.log("Error:", error.message);
+		exit(1);
+	} finally {
+		exit(0);
+	}
+})();
