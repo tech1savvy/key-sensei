345647afa7bf997727e8e14b2e070cd43d10a0c6
tech1savvy <amankumar010604@gmail.com>
2025-12-07 16:27:45 +0530

    feat(typing-test): register routes in app

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const { authRoutes } = require("./features/auth/auth.routes");
 const { protect, optionalProtect } = require("./features/auth/auth.middleware");
+const { authRoutes } = require("./features/auth/auth.routes");
+const {
+	typingTestRoutes,
+} = require("./features/typingTest/typingTest.routes.js");
 
 const app = express();
 
@@ -14,6 +17,9 @@ app.get("/api", optionalProtect, (_, res) => {
 	res.json("hello from key-sensie!");
 });
 
+// TODO: Create a seprate routes.js
+
 app.use("/api/auth", authRoutes);
+app.use("/api/typing-test", protect, typingTestRoutes);
 
 module.exports = app;
