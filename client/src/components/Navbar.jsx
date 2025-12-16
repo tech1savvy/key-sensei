1f78c6824de892df3534e630b3694da0bd6f0c0a
tech1savvy <amankumar010604@gmail.com>
2025-12-16 16:06:52 +0530

    feat(auth): add login and register urls to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,23 @@ import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-body-tertiary p-1">
-      <Link to="/" className="navbar-brand">
-        <i className="bi bi-keyboard p-1"></i>
-        Key Sensei
-      </Link>
+    <nav className="bg-body-tertiary p-2">
+      <div className="container">
+        <div className="row">
+          <Link to="/" className="col-10 navbar-brand">
+            <i className="bi bi-keyboard p-1"></i>
+            Key Sensei
+          </Link>
+          <div className="row col-2">
+            <Link to="/login" className="col nav-link">
+              Login
+            </Link>
+            <Link to="/register" className="col nav-link">
+              Register
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
