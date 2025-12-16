941d76f0565341dd6fbe05d6e467abb897015598
tech1savvy <amankumar010604@gmail.com>
2025-12-16 16:10:20 +0530

    feat(auth): register login and register routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Login from "./components/Auth/Login.jsx";
+import Register from "./components/Auth/Register.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 import PageNotFound from "./components/Pages/PageNotFound.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
   { path: "*", element: <PageNotFound /> },
 ]);
 
