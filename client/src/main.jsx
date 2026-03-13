801fb58bb14a6606933237f86ea4b3d13074c1b5
tech1savvy <amankumar010604@gmail.com>
2026-03-13 07:30:25 +0530

    feat(auth): wrap app with auth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,21 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Login from "./components/Auth/Login.jsx";
-import Register from "./components/Auth/Register.jsx";
+import LoginForm from "./components/Auth/LoginForm.jsx";
+import RegisterForm from "./components/Auth/RegisterForm.jsx";
+import { AuthProvider } from "./contexts/AuthProvider.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 import PageNotFound from "./components/Pages/PageNotFound.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
+  { path: "/login", element: <LoginForm /> },
+  { path: "/register", element: <RegisterForm /> },
   { path: "*", element: <PageNotFound /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
