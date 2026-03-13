72e7d3913ce69f545dd3d91883c35093b0da7781
tech1savvy <amankumar010604@gmail.com>
2026-03-13 07:30:05 +0530

    feat(auth): add vite proxy for API requests

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
   preview: {
     allowedHosts: [
       'key-sensei-client.onrender.com'
